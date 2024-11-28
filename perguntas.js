8e00359370a3d3dd290e248af28dd9afd08057b2
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-28 17:17:11 -0300

    maria

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória para armazenar valores.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'Um loop é uma estrutura que repete um bloco de código enquanto uma condição for verdadeira.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em inglês é thank you.'
+)
